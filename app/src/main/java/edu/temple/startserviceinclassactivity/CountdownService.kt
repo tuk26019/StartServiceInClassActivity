eed51cbfc9bb10c1388a284e3173199aa0126e44
tuk26019 <tuk26019@temple.edu>
2024-04-04 12:14:01 -0400

    created a CountdownService

---
+++ app/src/main/java/edu/temple/startserviceinclassactivity/CountdownService.kt
@@ -0,0 +1,34 @@
+package edu.temple.startserviceinclassactivity
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+const val COUNTDOWN_START_VALUE="countdownValue"
+class CountdownService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        intent?.run{
+            val from = getIntExtra(COUNTDOWN_START_VALUE,10)
+
+            CoroutineScope(Dispatchers.IO).launch{
+                repeat(from){
+                    Log.d("Countdown", (from-it).toString())
+                    delay(1000)
+                }
+            }
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
--- app/src/main/java/edu/temple/startserviceinclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/startserviceinclassactivity/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.startserviceinclassactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,5 +15,10 @@ class MainActivity : AppCompatActivity() {
 
         val countdownStartButton = findViewById<Button>(R.id.countdownButton)
 
+
+        countdownStartButton.setOnClickListener{
+            startService(Intent(this, CountdownService::class.java)
+                .putExtra(COUNTDOWN_START_VALUE, countdownValue.text.toString().toInt()))
+        }
     }
 }
